096d91da523c2a671999cbc4c7388a3c39f511fb
su-kang <sanguk.kang@cj.net>
2024-12-30 17:34:22 +0900

    context 예제 추가

---
+++ src/components/AuthContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+
+interface AuthProps {
+  isAuth?: boolean | null | undefined;
+  setIsAuth?: (isAuth: boolean) => void;
+}
+export const AuthContext: any = createContext<AuthProps>({ isAuth: false });
+
+const AuthProvider = ({ isAuthProp = false, children }: any) => {
+  const [isAuth, setIsAuth] = useState(isAuthProp);
+
+  return (
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+
+export const useAuth = () => {
+  const { isAuth, setIsAuth }: any = useContext(AuthContext);
+  return { isAuth, setIsAuth };
+};
--- src/pages/ReactHook.tsx
+++ src/pages/ReactHook.tsx
@@ -1,50 +1,49 @@
-import { createContext, forwardRef, useContext, useEffect, useImperativeHandle, useRef, useState } from 'react';
-
-const Context = createContext<{ hello: string } | undefined>(undefined);
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import { useAuth } from "../components/AuthContext";
 
 const ReactHook = () => {
-	const [context, setContext] = useState('react context');
-	const handlerRef = useRef<HTMLFormElement>(null);
+  const handlerRef = useRef<HTMLFormElement>(null);
 
-	useEffect(() => {
-		console.log('>> ReactHook Mount');
-		return () => {
-			console.log('>> ReactHook Unmount');
-		};
-	}, []);
+  useEffect(() => {
+    console.log(">> ReactHook Mount");
+    return () => {
+      console.log(">> ReactHook Unmount");
+    };
+  }, []);
 
-	const chgContext = () => {
-		setContext(context => context + ' add');
-	};
+  const onClickChildEvent = () => {
+    handlerRef.current?.addNumber();
+  };
 
-	const onClickChildEvent = () => {
-		handlerRef.current?.addNumber();
-	};
-
-	return (
-		<>
-			<Context.Provider value={{ hello: context }}>
-				<div>
-					<h2>ReactHook Test</h2>
-					<div className="box">
-						<SateComponent />
-					</div>
-					<div className="box">
-						<ContextComponent />
-						<button onClick={() => chgContext()}>change context</button>
-						<button onClick={() => setContext('react context')}>init context</button>
-					</div>
-					<div className="box">
-						<RefComponent />
-					</div>
-					<div className="box">
-						<ImperativeHandleComponent ref={handlerRef} name="ImperativeHandle Component" />
-						<button onClick={onClickChildEvent}>Ref Click</button>
-					</div>
-				</div>
-			</Context.Provider>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <h2>ReactHook Test</h2>
+        <div className="box">
+          <SateComponent />
+        </div>
+        <div className="box">
+          <ContextComponent />
+        </div>
+        <div className="box">
+          <RefComponent />
+        </div>
+        <div className="box">
+          <ImperativeHandleComponent
+            ref={handlerRef}
+            name="ImperativeHandle Component"
+          />
+          <button onClick={onClickChildEvent}>Ref Click</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ReactHook;
@@ -54,26 +53,26 @@ export default ReactHook;
  * @returns
  */
 const SateComponent = () => {
-	const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
-	useEffect(() => {
-		console.log('>> ReactHook value update', value);
-		// 초기화
-		// return () => {
-		// 	console.log('>> ReactHook value clear');
-		// };
-	}, [value]);
+  useEffect(() => {
+    console.log(">> ReactHook value update", value);
+    // 초기화
+    // return () => {
+    // 	console.log('>> ReactHook value clear');
+    // };
+  }, [value]);
 
-	return (
-		<>
-			<h3>UseSateComponent</h3>
-			<div>{value}</div>
-			<div>
-				<button onClick={() => setValue(value + 1)}>Click</button>
-				<button onClick={() => setValue(0)}>Clear</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h3>UseSate Component</h3>
+      <div>{value}</div>
+      <div>
+        <button onClick={() => setValue(value + 1)}>Click</button>
+        <button onClick={() => setValue(0)}>Clear</button>
+      </div>
+    </>
+  );
 };
 
 /**
@@ -81,13 +80,15 @@ const SateComponent = () => {
  * @returns
  */
 const ContextComponent = () => {
-	const value = useContext(Context);
-	return (
-		<>
-			<h3>ChildComponent</h3>
-			<div>{value ? value.hello : ''}</div>
-		</>
-	);
+  const { isAuth, setIsAuth } = useAuth();
+
+  return (
+    <>
+      <h3>Context Component</h3>
+      <div>{isAuth ? "권한 존재" : "권한 미존재"}</div>
+      <button onClick={() => setIsAuth(!isAuth)}>Contenxt 변경</button>
+    </>
+  );
 };
 
 /**
@@ -95,42 +96,42 @@ const ContextComponent = () => {
  * @returns
  */
 const RefComponent = () => {
-	const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-	const onClickEvent = () => {
-		inputRef.current?.focus();
-	};
-	return (
-		<>
-			<h3>RefComponent</h3>
-			<div>
-				<input ref={inputRef} type="text" placeholder="canal frame test" />
-			</div>
-			<div>
-				<button onClick={onClickEvent}>focus</button>
-			</div>
-		</>
-	);
+  const onClickEvent = () => {
+    inputRef.current?.focus();
+  };
+  return (
+    <>
+      <h3>Ref Component</h3>
+      <div>
+        <input ref={inputRef} type="text" placeholder="canal frame test" />
+      </div>
+      <div>
+        <button onClick={onClickEvent}>focus</button>
+      </div>
+    </>
+  );
 };
 
 /**
  * useImperativeHandle 예제
  */
 const ImperativeHandleComponent = forwardRef((props: any, ref: any) => {
-	const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
 
-	const addNumber = () => {
-		setValue(value + 1);
-	};
+  const addNumber = () => {
+    setValue(value + 1);
+  };
 
-	useImperativeHandle(ref, () => ({ addNumber }));
+  useImperativeHandle(ref, () => ({ addNumber }));
 
-	return (
-		<>
-			<h3>ImperativeHandleComponent</h3>
-			<div>
-				{props.name} : {value}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h3>ImperativeHandle Component</h3>
+      <div>
+        {props.name} : {value}
+      </div>
+    </>
+  );
 });
