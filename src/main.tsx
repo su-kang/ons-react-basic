2131e7ab011d1beb446d670b319e85d7feb1d434
su-kang <sanguk.kang@cj.net>
2024-12-31 15:47:33 +0900

    오타 제거

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AuthProvider isAuthProp={false}>
-      <App />,
+      <App />
     </AuthProvider>
   </BrowserRouter>
 );
